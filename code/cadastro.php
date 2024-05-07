bf7899967f7a915b3d665039c26f6811a8fa73f1
Felipe-candido <felipepcandido8@gmail.com>
2024-05-07 14:22:45 -0300

    Implementando bootstrap no cadastro

--- code/cadastro.php
+++ code/cadastro.php
@@ -9,27 +9,22 @@ $validador->verificar_logado();
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/estilo.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-<body>
-    <div class="container">
-        <div class="box"> <img src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Logo_fatec_araras.png" alt=""><h2 class="textlogin">CADASTRE-SE</h2>
-        <form id="cadastro" action="cadastro.php" method="POST" onsubmit="return validarForm(event)">
-            <div class="form-group">
-                <label for="nome">Nome:</label>
-                <input type="text" id="nome" name="nome" placeholder="Insira seu nome"required><br><br>
-            </div>
-            <div class="form-group">
-                <label for="curso">Curso (DSM=1 || GE=2):</label>
-                <input type="text" id="curso" name="curso" placeholder="Insira o numero do seu curso"required><br><br>
-            </div>
-            <input class="button" type="submit" value="Entrar">
-        </form>
-        </div>
-    </div>
-
-    <a href="home.php">VOLTAR</a>
+<body class="text-center">
+    <form id="cadastro" action="cadastro.php" class="form-signin" method="POST" onsubmit="return validarForm(event)">
+        <img class="mb-4" src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Logo_fatec_araras.png" alt="logo" width="200" height="100">
+        <h1 class="h3 mb-3 font-weight-normal">Cadastre-se</h1>
+        <label for="nome" class="sr-only">Nome</label>
+        <input type="text" id="nome" name="nome" class="form-control" placeholder="Insira seu nome"required>
+        <label for="curso" class="sr-only">Curso (DSM=1 || GE=2)</label>
+        <input type="text" id="curso" name="curso" class="form-control" placeholder="Numero do seu curso"required>
+        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Entrar">
+        <a class="btn btn-lg btn-primary btn-block" href="home.php">Voltar</a>
+    </form>
+    
+    
 </body>
 </html>
 
