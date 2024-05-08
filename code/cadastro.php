17a0812b39f1f411c61e6e1262cf13e958be7994
Felipe-candido <felipepcandido8@gmail.com>
2024-05-07 22:38:14 -0300

    Arrumando validação do foms com JS

--- code/cadastro.php
+++ code/cadastro.php
@@ -13,13 +13,13 @@ $validador->verificar_logado();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body class="text-center">
-    <form id="cadastro" action="cadastro.php" class="form-signin" method="POST" onsubmit="return validarForm(event)">
+    <form id="cadastro" action="cadastro.php" class="form-signin" method="POST">
         <img class="mb-4" src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Logo_fatec_araras.png" alt="logo" width="200" height="100">
         <h1 class="h3 mb-3 font-weight-normal">Cadastre-se</h1>
         <label for="nome" class="sr-only">Nome</label>
-        <input type="text" id="nome" name="nome" class="form-control" placeholder="Insira seu nome"required>
+        <input type="text" id="nome" name="nome" class="form-control" placeholder="Insira seu nome">
         <label for="curso" class="sr-only">Curso (DSM=1 || GE=2)</label>
-        <input type="text" id="curso" name="curso" class="form-control" placeholder="Numero do seu curso (DSM=1 || GE=2)"required>
+        <input type="text" id="curso" name="curso" class="form-control" placeholder="Numero do seu curso (DSM=1 || GE=2)">
         <input class="btn btn-lg btn-success btn-block" type="submit" value="Entrar">
         <a class="btn btn-lg btn-primary btn-block" href="home.php">Voltar</a>
     </form>
@@ -28,19 +28,18 @@ $validador->verificar_logado();
 
 <script>
         // FUNÇÃO PARA VALIDAR O PREENCHIMENTO E ENVIO DO FORMULÁRIO //
-        function validarForm(event){
-            var form = document.getElementById('cadastro');
+        document.getElementById('cadastro').addEventListener('submit', function(event) {
             var nome = document.getElementById('nome').value.trim();
             var curso = document.getElementById('curso').value.trim();
-            
+                
             // VERIFICA SE HÁ ALGUM CAMPO VAZIO // 
-            if(nome.trim() === '' || ra.trim() === '' || placa.trim() === ''){
-                alert('Por favor, preencha todos os campos.');
-                return false; // IMPEDE O ENVIO DO FORM // 
+            if(nome === '' || curso === '' || curso != '1' && curso != '2'){
+                alert('Por favor, preencha todos os campos corretamente.');
+                event.preventDefault(); // IMPEDE O ENVIO DO FORM // 
+            } else {
+                alert('Cadastro realizado!');
             }
-            alert('Cadastro realizado!');
-            return true; // PERMITE O ENVIO DO FORM
-        }
+        });
 </script>
 
 <?php
